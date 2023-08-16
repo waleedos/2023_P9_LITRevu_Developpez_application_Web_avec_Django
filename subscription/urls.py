20fb027e56148a9c86889268a463484ba4137cfe
EL-WALID EL-KHABOU <alkhabou@gmail.com>
2023-08-16 10:39:58 +0200

    Nettoyage globale

--- subscription/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('subscription-management/', views.SubscriptionManagementView.as_view(), name='subscription_management'),
-]
--- ticketing/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from . import models
-
-
-admin.site.register(models.Ticket)
-admin.site.register(models.Review)
